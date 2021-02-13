502b8ac5baa36e96d8ec7dea25c36d8291243fe5
lmanesh7 <47899760+lmanesh7@users.noreply.github.com>
2021-02-13 18:54:30 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -13,7 +13,7 @@ if file1 is None:
 col1, col2 = st.beta_columns(2)
 if file is not None:
     df = pd.read_csv(file)
-    df['z'] = pd.to_datetime(df['Created Date'], format="%d-%m-%Y %H:%M:%S")
+    df['z'] = pd.to_datetime(df['Created Date'])
     l = []
     a = pd.Series([])
     df1 = pd.Series([])
